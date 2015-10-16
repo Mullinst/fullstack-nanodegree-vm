725e7d2cc1ec6d657392a68eaaf46f1b7bdc0629
Mullinst <mullinst2011@gmail.com>
2015-10-16 13:18:07 -0400

    fix: Vagrant errors

--- vagrant/tournament/tournament.py
+++ vagrant/tournament/tournament.py
@@ -16,25 +16,50 @@ def connect():
 
 def deleteMatches():
     """Remove all the match records from the database."""
+    DB = connect()
+    cursor = DB.cursor()
+    query = ""  # Delete all rows in match table
+    cursor.execute(query)
+    cursor.commit()
+    DB.close()
 
 
 def deletePlayers():
     """Remove all the player records from the database."""
+    DB = connect()
+    cursor = DB.cursor()
+    query = ""  # Delete all rows in players table
+    cursor.execute(query)
+    cursor.commit()
+    DB.close()
 
 
 def countPlayers():
     """Returns the number of players currently registered."""
+    DB = connect()
+    cursor = DB.cursor()
+    query = ""  # count aggregate all names in players
+    count = cursor.execute(query)
+    cursor.commit()
+    DB.close()
+    return count
 
 
 def registerPlayer(name):
     """Adds a player to the tournament database.
-  
+
     The database assigns a unique serial id number for the player.  (This
     should be handled by your SQL database schema, not in your Python code.)
-  
+
     Args:
       name: the player's full name (need not be unique).
     """
+    DB = connect()
+    cursor = DB.cursor()
+    query = ""  # Insert name into players table
+    cursor.execute(query)
+    cursor.commit()
+    DB.close()
 
 
 def playerStandings():
@@ -59,16 +84,16 @@ def reportMatch(winner, loser):
       winner:  the id number of the player who won
       loser:  the id number of the player who lost
     """
- 
- 
+
+
 def swissPairings():
     """Returns a list of pairs of players for the next round of a match.
-  
+
     Assuming that there are an even number of players registered, each player
     appears exactly once in the pairings.  Each player is paired with another
     player with an equal or nearly-equal win record, that is, a player adjacent
     to him or her in the standings.
-  
+
     Returns:
       A list of tuples, each of which contains (id1, name1, id2, name2)
         id1: the first player's unique id
@@ -76,5 +101,3 @@ def swissPairings():
         id2: the second player's unique id
         name2: the second player's name
     """
-
-
--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -8,5 +8,34 @@
 --
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
+CREATE DATABASE tournament; 
 
+-- Remove drop when schema set
+DROP TABLE IF EXISTS players CASCADE;
+DROP TABLE IF EXISTS tournaments CASCADE;
+DROP TABLE IF EXISTS matches CASCADE;
+DROP TABLE IF EXISTS results table_name CASCADE;
 
+-- Define tables
+CREATE TABLE players (
+	player_id SERIAL PRIMARY KEY,
+	name TEXT
+);
+
+CREATE TABLE tournaments (
+	tournament_id INTEGER PRIMARY KEY,
+	participant INTEGER REFERENCES players (player_id)
+);
+
+CREATE TABLE matches (
+	match_id SERIAL PRIMARY KEY,
+	tournament_id INTEGEREGER REFERENCES tournaments (tournament_id),
+	player1 INTEGER REFERENCES players (player_id),
+	player2 INTEGER REFERENCES players (player_id)
+);
+
+CREATE TABLE results (
+	player_id INTEGER REFERENCES players (player_id),
+	match_id INTEGER REFERENCES matches (player_id),
+	result TEXT
+);
